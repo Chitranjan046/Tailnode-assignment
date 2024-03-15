0ed5582609781b5969616a3ab97670cc87319c77
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-15 13:56:13 +0530

    Complete Part B (Scrape and Store to the database )

--- part-B.py
+++ part-B.py
@@ -1,4 +1,5 @@
 import pymongo
+from bs4 import BeautifulSoup
 import requests
 import os
 from dotenv import load_dotenv
@@ -15,4 +16,50 @@ def database_collection():
     db = client.tailnode  # Use your database name
     collection = db.books
     return collection
+# Function to scrape and store data into MongoDB Atlas database.
+def scrape_and_store_data():
+    base_url = "http://books.toscrape.com/catalogue/page-{}.html"
+    collection = database_collection()
+    
+    for page in range(1, 51):  # Scrape all 50 pages
+        url = base_url.format(page)
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+        
+        print("Scraping page:", page)
 
+        for book in soup.find_all('article', class_='product_pod'):
+            img_url = book.find('img')['src']  # Extract image URL
+            
+            # Extract book name from different header tags
+            name = None
+            for header_tag in ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']:
+                if book.find(header_tag) and book.find(header_tag).a:
+                    name = book.find(header_tag).a['title']
+                    break
+            if not name:
+                name = "Unknown"
+            
+            price = book.find('p', class_='price_color').text
+            availability = book.find('p', class_='instock availability').text.strip()
+            rating = book.find('p', class_='star-rating')['class'][1]
+
+            # Print the scraped data attributes in the console
+            print("Image URL:", img_url)
+            print("Name:", name)
+            print("Price:", price)
+            print("Availability:", availability)
+            print("Rating:", rating)
+            
+            # Insert data into MongoDB Atlas
+            result = collection.insert_one({
+                "image_url": img_url,  # Include image URL in MongoDB document
+                "name": name,
+                "price": price,
+                "availability": availability,
+                "rating": rating,
+            })
+            print("Inserted book:", result.inserted_id)
+
+if __name__ == "__main__":
+    scrape_and_store_data()
