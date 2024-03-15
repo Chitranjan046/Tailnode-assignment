923563af72b02f89a9af079cd48b501eaed119cb
Chitranjan  Kumar Patel <chitranjankumarpatel8@gmail.com>
2024-03-15 12:27:21 +0530

    Complete posts logic and store in database.

--- part-A.py
+++ part-A.py
@@ -14,6 +14,7 @@ API_KEY = os.getenv("API_KEY")
 client = pymongo.MongoClient(MONGODB_URL)
 db = client.tailnode
 user_collection = db.users
+post_collection = db.posts
 
 # Insert User Data into Database
 r = requests.get("https://dummyapi.io/data/v1/user", headers={"app-id": API_KEY}).json()
@@ -21,3 +22,12 @@ user_data = r['data']
 
 result = user_collection.insert_many(user_data)
 print("User Data inserted successfully", result.inserted_ids)
+
+
+# Insert Posts Data into Database
+
+for user in user_data:
+    r = requests.get(f"https://dummyapi.io/data/v1/user/{user['id']}/post", headers={'app-id': API_KEY}).json()
+    post_data = r['data']
+    result = post_collection.insert_many(post_data)
+    print("Post Data inserted successfully", result.inserted_ids)
